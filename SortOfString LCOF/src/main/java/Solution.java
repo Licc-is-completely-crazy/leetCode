7ee652adcccbd1e9701d806b7307ee8af89e4d91
lichengcheng <chengcheng.li3@zhenai.com>
2020-08-02 09:50:18 +0800

    回溯

--- SortOfString LCOF/src/main/java/Solution.java
+++ SortOfString LCOF/src/main/java/Solution.java
@@ -1,13 +1,14 @@
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class Solution {
+    List<String> res = new LinkedList<>();
+    char[] c;
+
     public static void main(String[] args) {
         Solution solution=new Solution();
-        solution.permutation("zg");
+//        solution.permutation("zg");
 
+        solution.best("abcde");
     }
 
     public String[] permutation(String s) {
@@ -58,4 +59,33 @@ public class Solution {
         return res.get(s.length()-1);
 
     }
+
+
+    public String[] best(String s) {
+        c = s.toCharArray();
+        dfs(0);
+        return res.toArray(new String[res.size()]);
+    }
+    void dfs(int x) {
+        if(x == c.length - 1) {
+            System.out.println(String.valueOf(c));
+            res.add(String.valueOf(c)); // 添加排列方案
+            return;
+        }
+        HashSet<Character> set = new HashSet();
+        for(int i = x; i < c.length; i++) {
+            if(set.contains(c[i])) continue; // 重复，因此剪枝
+            set.add(c[i]);
+            swap(i, x); // 交换，将 c[i] 固定在第 x 位
+            dfs(x + 1); // 开启固定第 x + 1 位字符
+            swap(i, x); // 恢复交换
+        }
+    }
+    void swap(int a, int b) {
+        char tmp = c[a];
+        c[a] = c[b];
+        c[b] = tmp;
+    }
+
+
 }
